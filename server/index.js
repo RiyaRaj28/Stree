80cbe675e7e8278cce48ecdccb196d63f79259c5
RiyaRaj28 <rrajj7674@gmail.com>
2024-08-18 23:54:14 +0530

    minor fix

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 require("./db conn/connection");
 const router = require('./routes/userRouters');
 
 app.use(router);
-app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
