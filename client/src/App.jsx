c87f088032a0fc68ea42760dc1066e7f17c42c8c
Imrana Sumbul <143505827+imranasumbul@users.noreply.github.com>
2024-08-19 07:17:45 +0530

    initialized latest version of vite with react and installed other packages. (#2)
    * initialized vite with react and installed packages like axios, recoil and router-dom. Also set up tailwindcss
    * A minor correction related to Strict mode
    
    Authored-by: Imrana Sumbul

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        App.jsx
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+)
