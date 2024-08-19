8e960079f119b0a6af34c208970bab6d6edcfb9a
Imrana Sumbul <imranasumbul1211@gmail>
2024-08-19 23:17:47 +0530

    small bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RecoilRoot } from 'recoil';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
