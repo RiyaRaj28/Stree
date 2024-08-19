4be6ad6359cbfc69ba6d521df86e51ed86c5d7ea
Imrana Sumbul <imranasumbul1211@gmail>
2024-08-19 23:12:03 +0530

    Made sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Signup from './components/auth/Signup'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        App.jsx
+        <Signup />
       </div>
     </>
   )
